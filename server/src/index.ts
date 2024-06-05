d3edfaf65f942209b4976a06d4d0ea207a766fa6
mahmoud.suleiman <95286033+msuleiman-Dev@users.noreply.github.com>
2024-06-05 17:03:29 +0100

    added server code and packages - refactored client code - both server and client apps are running

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import mocks from "./mock";
+import typeDefs from "./schema";
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+      🚀  Server is running!
+      📭  Query at ${url}
+    `);
+};
+
+startApolloServer();
